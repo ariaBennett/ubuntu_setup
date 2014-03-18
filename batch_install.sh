dcd2e1d07856704abbbb5693187bb7687a181b8c
Aria Bennett <twoplustwo@gmail.com>
2014-03-18 16:17:38 -0700

    Added meteor, dvd support, comments on blocking.

--- batch_install.sh
+++ batch_install.sh
@@ -5,6 +5,7 @@ sudo apt-get install git
 sudo apt-get install nodejs
 sudo apt-get install npm
 sudo npm -g install nodemon
+sudo npm -g install meteor
 
 #Coffeescript
 sudo apt-get install coffeescript
@@ -12,6 +13,11 @@ sudo apt-get install coffeescript
 #Vim
 sudo apt-get install vim-gtk
 sudo apt-get install nodejs
+
+#DVD support
+sudo /usr/share/doc/libdvdread4/install-css.sh
+
+### Place apps which have blocking installs below here.
 
 #Flash player
 sudo apt-get install ubuntu-restricted-extras
