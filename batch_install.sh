aff33d92d411b05f7e8720afab3de35f266015af
Aria Bennett <twoplustwo@gmail.com>
2014-03-18 14:12:32 -0700

    Added batch_install.sh script

---
+++ batch_install.sh
@@ -0,0 +1,12 @@
+#Node
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm -g install nodemon
+
+#Coffeescript
+sudo apt-get install coffeescript
+
+#Vim
+sudo apt-get install vim-gtk
+sudo apt-get install nodejs
+
